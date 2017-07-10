7ce97c69b3b6d3fd08b552b63879c0654d780956
Amed Kino <amed@xbyte.team>
2017-07-10 10:34:05 +0200

    Refactor WelcomeMessage and HelpBlock

---
+++ src/helpBlock.js
@@ -0,0 +1,8 @@
+require('colors')
+
+console.log('Space   '.bold, '- Start-Stop recording')
+console.log('P       '.bold, '- Play the recorded video')
+console.log('F       '.bold, '- Filter out the record')
+console.log('A       '.bold, '- Submit the record')
+console.log('H       '.bold, '- Help')
+console.log('Ctrl + C'.bold, '- To exit beichtstuhl-pp')
--- src/welcomeMessage.js
+++ src/welcomeMessage.js
@@ -1,11 +1,7 @@
-var colors = require('colors');
-console.log('\n     -------------------------------------------------------'.rainbow)
-console.log('       You are running BEICHTSTUHL Playback&Processor tool'.cyan.bold)
-console.log('     -------------------------------------------------------\n'.rainbow)
+require('colors')
 
-console.log('You can press one of the keyes:')
-console.log('space'.bold, 'To start recording')
-console.log('s'.bold, 'To stop recording')
-console.log('p'.bold, 'To play the requested video')
-console.log('h'.bold, 'To get help list')
-console.log('ctrl + c'.bold, 'To exit beichtstuhl-pp')
+console.log('\n     -----------------------------------------------------'.rainbow)
+console.log('      You are running BEICHTSTUHL Playback&Processor tool'.cyan.bold)
+console.log('     -----------------------------------------------------\n'.rainbow)
+
+require('./helpBlock.js')
